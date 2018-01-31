3637e9a3ffdf4e720d4c246c5b0b6f17533f7385
Ryan Shaw <ryan@rshaw.me>
2018-01-31 22:10:42 +0000

    Update garlicore

--- index.js
+++ index.js
@@ -1,70 +1,70 @@
 'use strict';
 
-var litecore = module.exports;
+var garlicore = module.exports;
 
 // module information
-litecore.version = 'v' + require('./package.json').version;
-litecore.versionGuard = function(version) {
+garlicore.version = 'v' + require('./package.json').version;
+garlicore.versionGuard = function(version) {
   if (version !== undefined) {
-    var message = 'More than one instance of litecore-lib found. ' +
-      'Please make sure to require litecore-lib and check that submodules do' +
-      ' not also include their own litecore-lib dependency.';
+    var message = 'More than one instance of garlicore-lib found. ' +
+      'Please make sure to require garlicore-lib and check that submodules do' +
+      ' not also include their own garlicore-lib dependency.';
     throw new Error(message);
   }
 };
-litecore.versionGuard(global._litecore);
-global._litecore = litecore.version;
+garlicore.versionGuard(global._garlicore);
+global._garlicore = garlicore.version;
 
 // crypto
-litecore.crypto = {};
-litecore.crypto.BN = require('./lib/crypto/bn');
-litecore.crypto.ECDSA = require('./lib/crypto/ecdsa');
-litecore.crypto.Hash = require('./lib/crypto/hash');
-litecore.crypto.Random = require('./lib/crypto/random');
-litecore.crypto.Point = require('./lib/crypto/point');
-litecore.crypto.Signature = require('./lib/crypto/signature');
+garlicore.crypto = {};
+garlicore.crypto.BN = require('./lib/crypto/bn');
+garlicore.crypto.ECDSA = require('./lib/crypto/ecdsa');
+garlicore.crypto.Hash = require('./lib/crypto/hash');
+garlicore.crypto.Random = require('./lib/crypto/random');
+garlicore.crypto.Point = require('./lib/crypto/point');
+garlicore.crypto.Signature = require('./lib/crypto/signature');
 
 // encoding
-litecore.encoding = {};
-litecore.encoding.Base58 = require('./lib/encoding/base58');
-litecore.encoding.Base58Check = require('./lib/encoding/base58check');
-litecore.encoding.BufferReader = require('./lib/encoding/bufferreader');
-litecore.encoding.BufferWriter = require('./lib/encoding/bufferwriter');
-litecore.encoding.Varint = require('./lib/encoding/varint');
+garlicore.encoding = {};
+garlicore.encoding.Base58 = require('./lib/encoding/base58');
+garlicore.encoding.Base58Check = require('./lib/encoding/base58check');
+garlicore.encoding.BufferReader = require('./lib/encoding/bufferreader');
+garlicore.encoding.BufferWriter = require('./lib/encoding/bufferwriter');
+garlicore.encoding.Varint = require('./lib/encoding/varint');
 
 // utilities
-litecore.util = {};
-litecore.util.buffer = require('./lib/util/buffer');
-litecore.util.js = require('./lib/util/js');
-litecore.util.preconditions = require('./lib/util/preconditions');
+garlicore.util = {};
+garlicore.util.buffer = require('./lib/util/buffer');
+garlicore.util.js = require('./lib/util/js');
+garlicore.util.preconditions = require('./lib/util/preconditions');
 
 // errors thrown by the library
-litecore.errors = require('./lib/errors');
+garlicore.errors = require('./lib/errors');
 
 // main bitcoin library
-litecore.Address = require('./lib/address');
-litecore.Block = require('./lib/block');
-litecore.MerkleBlock = require('./lib/block/merkleblock');
-litecore.BlockHeader = require('./lib/block/blockheader');
-litecore.HDPrivateKey = require('./lib/hdprivatekey.js');
-litecore.HDPublicKey = require('./lib/hdpublickey.js');
-litecore.Networks = require('./lib/networks');
-litecore.Opcode = require('./lib/opcode');
-litecore.PrivateKey = require('./lib/privatekey');
-litecore.PublicKey = require('./lib/publickey');
-litecore.Script = require('./lib/script');
-litecore.Transaction = require('./lib/transaction');
-litecore.URI = require('./lib/uri');
-litecore.Unit = require('./lib/unit');
+garlicore.Address = require('./lib/address');
+garlicore.Block = require('./lib/block');
+garlicore.MerkleBlock = require('./lib/block/merkleblock');
+garlicore.BlockHeader = require('./lib/block/blockheader');
+garlicore.HDPrivateKey = require('./lib/hdprivatekey.js');
+garlicore.HDPublicKey = require('./lib/hdpublickey.js');
+garlicore.Networks = require('./lib/networks');
+garlicore.Opcode = require('./lib/opcode');
+garlicore.PrivateKey = require('./lib/privatekey');
+garlicore.PublicKey = require('./lib/publickey');
+garlicore.Script = require('./lib/script');
+garlicore.Transaction = require('./lib/transaction');
+garlicore.URI = require('./lib/uri');
+garlicore.Unit = require('./lib/unit');
 
 // dependencies, subject to change
-litecore.deps = {};
-litecore.deps.bnjs = require('bn.js');
-litecore.deps.bs58 = require('bs58');
-litecore.deps.Buffer = Buffer;
-litecore.deps.elliptic = require('elliptic');
-litecore.deps.scryptsy = require('scryptsy');
-litecore.deps._ = require('lodash');
+garlicore.deps = {};
+garlicore.deps.bnjs = require('bn.js');
+garlicore.deps.bs58 = require('bs58');
+garlicore.deps.Buffer = Buffer;
+garlicore.deps.elliptic = require('elliptic');
+garlicore.deps.scryptsy = require('scryptsy');
+garlicore.deps._ = require('lodash');
 
 // Internal usage, exposed for testing/advanced tweaking
-litecore.Transaction.sighash = require('./lib/transaction/sighash');
+garlicore.Transaction.sighash = require('./lib/transaction/sighash');
